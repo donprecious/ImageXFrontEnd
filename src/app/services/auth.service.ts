282ac2acb4dd599d54816e26d2e6a646cc74c3f1
nonyenathal <nonyeachus@outlook.com>
2020-04-26 17:03:00 +0100

    new changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -67,6 +67,7 @@ export class AuthService {
     return this.http.get<IResponseModel>(this.api.baseApiUrl+`Auth/ConfirmEmail/confirm?userId=${email}&token=${token}`);
   }
 
+
   public SetAuthLocalStorage(a: SiginResponseModel){
     localStorage.setItem('token', a.data.auth_token);
     localStorage.setItem('userId', a.data.user.id);
